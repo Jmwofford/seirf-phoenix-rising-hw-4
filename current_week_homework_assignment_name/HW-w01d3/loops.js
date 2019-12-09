aa79d609881abb88fa90a9620ff607887fe1da0b
LocalPlacesWith <LocalPlacesWith@gmail.com>
2019-12-08 20:01:25 -0800

    finished loop practice

---
+++ current_week_homework_assignment_name/HW-w01d3/loops.js
@@ -0,0 +1,30 @@
+// Create a loop that logs the numbers from 0-99 (ascending)
+for (i = 0; i < 100; i++) {
+    console.log(i)
+}
+
+// Create a loop that logs the EVEN numbers from 0-98 (ascending)
+for (i = 0 ; i < 100; i++) {
+    if (i % 2 == 0) {
+    console.log(i)
+    }
+}
+
+// Create a loop that logs the numbers from 49-72 (ascending)
+for (i = 49; i < 73; i++) {
+    console.log(i)
+}
+
+// Create a loop that logs the ODD numbers from 0-99 (ascending)
+for (i = 0 ; i < 100; i++) {
+    if (i % 2 !== 0) {
+    console.log(i)
+    }
+}
+
+// Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+for (i = 3 ; i < 91; i++) {
+    if (i % 3 == 0) {
+    console.log(i)
+    }
+}
